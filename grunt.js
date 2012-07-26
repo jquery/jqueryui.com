3130e32b7d6af924612c27d543c41a4b4c452674
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-26 15:51:27 +0200

    Placeholder for download page generator

--- grunt.js
+++ grunt.js
@@ -38,6 +38,20 @@ grunt.initConfig({
 	}, grunt.file.readJSON( "config.json" ) )
 });
 
+// TODO make this work, then include it in the build-wordpress alias below
+// requires actually publishing the download.jqueryui.com module and adding it to package.json
+grunt.registerTask( "build-download", function() {
+	var path = require( "path" ),
+		dir = path.dirname( require.resolve( "download.jqueryui.com" ) ),
+		markup = require("download.jqueryui.com")();
+	grunt.file.write( grunt.config( "wordpress.dir" ) + "/posts/page/download.html", "<script>{\n \"title\": \"Downloadbuilder\"\n}</script>" + markup );
+	var resources = grunt.file.expandFiles( dir + "/app/**" );
+	resources.forEach(function( file ) {
+		grunt.file.copy( file, file.replace( dir + "/app", "dist" ) );
+	});
+	grunt.log.write( "Wrote page/download.html and " + resources.length + " resources." );
+});
+
 grunt.registerTask( "default", "lint" );
 grunt.registerTask( "build-wordpress", "clean lint build-pages build-resources");
 grunt.registerTask( "deploy", "wordpress-deploy" );
